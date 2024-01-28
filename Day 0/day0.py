10a22a4eb32d82fade35d32f4f6008db020e56c9
giorgiiiii123 <amiridzegiorgi172@gmail.com>
2024-01-28 16:17:57 +0400

    first commit

---
+++ Day 0/day0.py
@@ -0,0 +1,41 @@
+from turtle import*
+
+forward(200)
+#we want to paint a house
+
+speed (30)
+width(7)
+color("purple")
+forward(200)       
+left(90)
+left(90)
+left(90)
+
+
+forward(200)
+left(90)
+
+forward(200)
+eft(90)l
+
+#drawing a door
+
+
+forward (70)
+color(yellow)
+    left(90)
+         forward(120)
+right(90)
+forward(120)
+
+
+
+(penup)
+goto(200,200)
+pendown
+
+
+right(120)
+
+exitonclick()
+         
