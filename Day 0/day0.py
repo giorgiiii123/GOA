39ed2ab66a7cc128bb1b0f8d21dd8ef81bae0f2a
giorgiiiii123 <amiridzegiorgi172@gmail.com>
2024-02-05 13:44:56 +0400

    updatr

--- Day 0/day0.py
+++ Day 0/day0.py
@@ -1,41 +1,69 @@
 from turtle import*
 
-forward(200)
+
 #we want to paint a house
 
 speed (30)
 width(7)
 color("purple")
-forward(200)       
-left(90)
-left(90)
-left(90)
-
-
+      
 forward(200)
 left(90)
-
 forward(200)
-eft(90)l
+left(90)
+forward(200)
+left(90)
+forward(200)
 
-#drawing a door
-
-
-forward (70)
-color(yellow)
-    left(90)
-         forward(120)
+penup()
+left(180)
+forward(200)
+pendown()
+left(-30)
+forward(100)
+left(-60)
+forward(100)
+left(-60)
+forward(100)
+left(-30)
+penup()
+forward(100)
+left(-90)
+forward(90)    
+pendown()
+left(-90)
+forward(50)
+left(-90)
+forward(90)
+left(-90)
+forward(50)
+left(90)
+forward(-90)
+penup()
 right(90)
-forward(120)
-
-
-
-(penup)
-goto(200,200)
-pendown
-
-
-right(120)
-
+left(-90)
+forward(30)
+pendown()
+forward(80)
+left(-90)
+forward(50)
+left(-90)
+forward(80)
+right(90)
+forward(50)
+penup()
+forward(30)
+pendown()
+right(90)
+left(90)
+forward(70)
+left(90)
+forward(50)
+left(90)
+forward(70)
+left (90)
+forward(50)
+        
 exitonclick()
-         
+
+
