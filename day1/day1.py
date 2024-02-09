069100a838da659c2e348263826d722b124fc941
giorgiiiii123 <amiridzegiorgi172@gmail.com>
2024-02-09 13:37:22 +0400

    davaleba 2

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+name="giorgi"
+age=13 
+adress="tbilisi 2 mikro 56 padiezdi 8 sartuli"
+print("gamarjoba me var "+name+" chemi asaki aris "+str(age))
+
+
+
+
+
+
+
+
+
+
+
---
+++ day2/day2.py
@@ -0,0 +1,12 @@
+name="davit"
+surname=" grdzelo"
+print("me davumegobrdi  "+name+surname+" dati chxikvadze")
+print("me goaze vfiqrob rom aris sauketeso onlain akademia visac programireba uyvars mattvis gansakutrebuli iqneba goa")
+
+
+
+
+
+
+
+
